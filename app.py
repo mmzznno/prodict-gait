bc2c6ed4f49781355ae3ebf5027ef4b2159085ca
mmzznno <mmzznno@yahoo.co.jp>
2022-11-26 14:14:15 +0900

    6th commit

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 from flask import Flask, render_template, request,redirect, url_for 
 from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
-from sklearn.tree import export_graphviz
-#from matplotlib.colors import ListedColormap
+from sklearn.metrics import accuracy_score
 
 app = Flask(__name__)
 
@@ -29,11 +28,8 @@ def index():
     
         message = "脳梗塞：退院前の歩行予測をするアプリです",
        )
-     #1データのチェックをした方がいいか？  
-     #2全データを入力していないと次の画面に進めないようにする    
+     #1データのチェックをした方がいいか？   
     
-    
-       
 #「/next」へアクセスがあった場合に、next.htmlを返す
 @app.route("/next", methods=['GET','POST'])
            
@@ -72,14 +68,20 @@ def next():
           }
       )
     #CSVファイルに退避　（計算部で使用）
-      #df.to_csv("/tmp/blood_data.csv",encoding='utf-8',index=False)
+      # ディレクトリがないとエラーになるため作成
+      #dir = Path
+      #direct = dir.mkdir(parents=True, exist_ok=True)
+      
+      
+      #df.to_csv('/tmp/blood_data.csv', encoding='utf-8',index=False)
+      
       df.to_csv("blood_data.csv",encoding='utf-8',index=False)         
      
       return redirect(url_for("next")) 
      
     return render_template('next.html',             
     title = "Gait Prodict",
-    message = "退院前の歩行予測をするアプリです 続き" ,)
+    message = "続き　退院前の歩行予測をするアプリです" ,)
 
    
 #「/back」へアクセスがあった場合に、index.htmlを返す   
@@ -98,13 +100,13 @@ def back():
 def sub():
   
   if request.method == "POST": 
-         
-      df_train = pd.read_csv("medical_data.csv",encoding='utf-8')
-      #print(df_train.head())
       
       blood = pd.read_csv("blood_data.csv",encoding='utf-8')
-      #blood = pd.read_csv("/tmp/blood_data.csv",encoding='utf-8')
+      #blood = pd.read_csv('/tmp/blood_data.csv', encoding='utf-8')
       #print(blood.head())
+         
+      df_train = pd.read_csv("medical_data.csv", encoding='utf-8')
+      #print(df_train.head())
       
     #追加・削除が発生する可能性があるため、1項目ずつ代入
       pati_id = blood.loc[:, ["pati_id"]]
@@ -164,12 +166,12 @@ def sub():
   df_gait = df_train.loc[:, ["GAIT"]]
   y  = df_gait
   y = y.dropna()
-  print(y.shape)
+  #print(y.shape)
   
   #訓練用とテストを分割する
   X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=0) 
   
-  print(len(X_train), len(X_test))
+  #print(len(X_train), len(X_test))
   
   model = LogisticRegression(penalty='none', max_iter=200)
   model.fit(X_train, y_train)
@@ -177,39 +179,85 @@ def sub():
   #精度の評価 
   y_pred1 = model.predict(X_test)
   cm = confusion_matrix(y_test, y_pred1)
-  cl = model.classes_
-  disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels= cl)
-  disp.plot()
+  
+  #正解率 
+  acc = (accuracy_score(y_test, y_pred1))
+  
+  #パーセント表示
+  acc_w = acc * 100
+  
+  text1 = "{:.1f}"
+  acc_dis_w = text1.format(acc_w)
+  
+  acc_dis = str(acc_dis_w) + "%"
+  
+  print("正解率")
+  print(acc_w)
+  
   
   #入力データの予測
   df_test =df_test.drop(["GAIT"] , axis=1)
   df_test =df_test.drop(["pati_id"] , axis=1)
   
-  print(X.head())
-  print(df_test)
+  #print(X.head())
+  #print(df_test)
+  
   df_test.to_csv("test_data.csv")
   y_pred = model.predict_proba(df_test)
+  
   print(y_pred)
   
+  #リストに変換
+  y_list = np.array(y_pred)
    
+  y_list1 =  y_list[0]
+  
+  print(len(y_list1))
+  
+  #最大値の確率を取得
+  y_list_max =  max(y_list1)
+  print(y_list_max)
+  
+  #最大値のINDEX
+  y_list_w = y_list1
+  
+  max_index = np.argmax(y_list_w)
+ 
+  print(max_index)
+  
+  #クラス数を取得  
   cl = model.classes_
   
-  #クラス分類の表示　0を含めると1から7までの段階で8分類
-  #例）分類が「4」であれば歩行段階は「5」
   
-  print(cl)
+  print(cl) 
+  print(cl[max_index])
   
+  #歩行のグレードを取得
+  grade_dis = cl[max_index]
   
-  #グラフを作成
-      
-  #  return redirect(url_for("sub")) 
+  grade_dis = str(grade_dis)
   
-    
+  #パーセント表示
+  proba = y_list_max * 100
+  
+  text = "{:.1f}"
+  proba_dis_w = text.format(proba)
+  
+  proba_dis = str(proba_dis_w) + "%"
+  
+  #楯列から横列へ変換
+  print(proba_dis)
+   
+     #return redirect(url_for("sub"))   
   return render_template("submit.html",
-      title = "Gait Prodict 予測結果です",
-      message = "y_pred",
-      )          
-
+                         
+      title = "Gait Prodict",
+      message = "あなたの退院前の歩行能力のグレード予測です" ,
+      
+      data1 = grade_dis, 
+      data2 = proba_dis,
+      data3 = acc_dis )
+      
 if __name__ == "__main__":
 
  app.run(debug=True)
